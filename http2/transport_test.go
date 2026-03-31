5ea58236ee34ae7edd63cf0b9bac0342107c2357
Nicholas S. Husin <nsh@golang.org>
2026-03-31 15:15:30 -0400

    http2: prevent hanging Transport due to bad SETTINGS frame
    This CL backports https://go.dev/cl/761581 to x/net.
    
    Fixes golang/go#78476
    Fixes CVE-2026-33814
    
    Change-Id: Ied435a51fdd8664d41dae14d082c39c76a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/net/+/761640
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Nicholas Husin <husin@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>

--- http2/transport_test.go
+++ http2/transport_test.go
@@ -5813,6 +5813,19 @@ func TestTransportTLSNextProtoConnImmediateFailureUnused(t *testing.T) {
 	}
 }
 
+func TestTransportDoNotHangOnZeroMaxFrameSize(t *testing.T) {
+	synctestTest(t, testTransportDoNotHangOnZeroMaxFrameSize)
+}
+func testTransportDoNotHangOnZeroMaxFrameSize(t testing.TB) {
+	tc := newTestClientConn(t)
+	tc.writeSettings(Setting{ID: SettingMaxFrameSize, Val: 0})
+	tc.wantFrameType(FrameSettings)
+
+	req, _ := http.NewRequest("POST", "https://dummy.tld/", strings.NewReader("body"))
+	tc.roundTrip(req)
+	// Previously, https://go.dev/issue/78476 caused an infinite hang here.
+}
+
 func TestExtendedConnectClientWithServerSupport(t *testing.T) {
 	setForTest(t, &disableExtendedConnectProtocol, false)
 	ts := newTestServer(t, func(w http.ResponseWriter, r *http.Request) {
